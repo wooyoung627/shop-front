c9cd5dd3a0549caadd224875b6ffa03f94b57a46
wooyoung627 <dldndud627@gmail.com>
2023-02-24 12:27:24 +0900

    install react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useAppSelector, useAppDispatch } from './app/hook';
 import Home from './views/Home';
 import UserRole from './const/UserRole';
 import { getUserReducer, initialStateUser, setUserReducer, User } from './reducers/userReducer';
+import SignIn from './views/sign/SignIn';
+import SignUp from './views/sign/SignUp';
 
 function App() {
   const user = useAppSelector((state) => state.user);
   const dispatch = useAppDispatch();
-  
+
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/sign-in' element={<SignIn/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
